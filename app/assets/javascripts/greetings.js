c4e7431b8dc954683966bde5f307307e364db5af
Fabio Gelsomini <fgelsomini@gmail.com>
2016-09-19 19:34:28 -0400

    Cleanup master

--- app/assets/javascripts/greetings.js
+++ app/assets/javascripts/greetings.js
@@ -1,2 +1,20 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+
+
+  $('#greeting-form').on('submit', function(event) {
+    event.preventDefault();
+    $.ajax({
+      url: "/greetings",
+      method: "post",
+      dataType: "html",
+      data: $(this).serialize()
+    }).done(function(returnedData) {
+      $('#greeting-list').prepend(returnedData);
+    });
+  });
+
+
+});
